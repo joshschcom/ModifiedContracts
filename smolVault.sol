c153d97b9a698d65ee6baa21404af3af2f0c6e47
joshschcom <84538564+joshschcom@users.noreply.github.com>
2022-08-22 15:39:05 +0200

    Update smolVault.sol

--- smolVault.sol
+++ smolVault.sol
@@ -7,17 +7,25 @@ contract ownVault is Ownable {
     IERC20 public DepositToken;     //We define our two tokens interacting with the contract
     IERC20 public VaultToken;       
 
-    mapping (address => uint) public depositAmount; //Every person who deposits an amount can be called with their address showing the amount deposited(uint)
-     
+    mapping (address => uint) public depositAmount;                     //Every person who deposits an amount can be called with their address showing the amount deposited(uint)
+    mapping (address => uint) public depositVTAmount;
 
-    constructor(address _DepositToken, address _VaultToken) {   //Deploying the contract with both of the token addresses
+    constructor(address _DepositToken, address _VaultToken) {           //Deploying the contract with both of the token addresses
         DepositToken = IERC20(_DepositToken);
         VaultToken = IERC20(_VaultToken);
     }
 
-    function deposit(uint _amount) public payable {             //a deposit function, with a number as amuount(IERC) which is public and can be paid with(payable)
+    function depositDepositToken(uint _amount) public payable {         //a deposit function, with a number as amuount(IERC) which is public and can be paid with(payable)
         DepositToken.transferFrom(msg.sender, address(this), _amount);  //"DepositToken is the Token transfered so that ETH wont be associated with this function
-        depositAmount[msg.sender] += _amount;                   //when this function is called add the amount to the user at mapping
+        depositDTAmount[msg.sender] += _amount;                         //when this function is called add the amount to the user at mapping
+    }
+
+    function depositVaultToken(uint _amount) public payable onlyOwner {
+        VaultToken.transferFrom(msg.sender, address(this), _amount);
+        depositVTAmount[msg.sender] += _amount;
     }
     
+    function withdrawVaultToken(uint _amount) external {
+        VaultToken.transfer(msg.sender, _amount);
+    }
 }
